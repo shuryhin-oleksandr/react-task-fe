b52a31739a3ac6b40b9a4cf361d70321dc30b458
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-10-03 22:34:59 +0300

    Install  react-router-dom. Add routing.

---
+++ src/pages/CreateTodo.js
@@ -0,0 +1,5 @@
+import {Typography} from "@mui/material";
+
+const CreateTodo = () => <Typography variant="h4" gutterBottom>Create Todo</Typography>
+
+export default CreateTodo
---
+++ src/pages/List.js
@@ -0,0 +1,57 @@
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+
+function createData(
+  status: string,
+  name: number,
+) {
+  return {status, name};
+}
+
+const rows = [
+  createData('Done', 'Feed the cat'),
+  createData('In progress', 'Wash the car'),
+  createData('To do', 'Call Denis'),
+  createData('In progress', 'Go to driving'),
+  createData('Expired', 'Make an estimation'),
+];
+
+const List = () => (
+  <>
+    <Typography variant="h4" gutterBottom>
+      ToDo List
+    </Typography>
+    <Button variant="contained" size="small" sx={{marginBottom: 3}}>Create</Button>
+    <TableContainer component={Paper}>
+      <Table sx={{minWidth: 650}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Staus</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+              <TableCell>
+                <Typography variant="body2" gutterBottom>{row.status}</Typography>
+              </TableCell>
+              <TableCell>
+                {row.name}
+              </TableCell>
+              <TableCell>
+                <Button variant="contained" size="small">Remove</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </>
+)
+
+export default List
