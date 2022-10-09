3c9c2b6ee0b3d9e54a259f3538737d981f86154e
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2022-10-09 22:45:34 +0300

    Make TodoForm to handle both create and update

--- src/features/todos/TodoCreatePage.js
+++ src/features/todos/TodoCreatePage.js
@@ -1,12 +1,12 @@
 import {Button, Typography} from "@mui/material";
 import * as React from "react";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import {useDispatch} from "react-redux";
-import {TodoAPI} from "./todoAPI";
 import {Field, Form, Formik} from "formik";
 import {TextField} from "formik-mui";
 import * as Yup from 'yup';
+import {TodoAPI} from "./todoAPI";
 
 const TodoCreatePage = () => {
   const navigate = useNavigate();
@@ -25,6 +25,7 @@ const TodoCreatePage = () => {
 
 export const TodoForm = (props) => {
   const dispatch = useDispatch()
+  const {todoId} = useParams();
 
   return (
     <Formik
@@ -39,13 +40,18 @@ export const TodoForm = (props) => {
           .min(3, 'Must be 3 characters or more')
           .max(200, 'Must be 200 characters or less'),
       })}
-      onSubmit={async (values, { setSubmitting }) => {
-        // Ask: how can I submit until request is done?
-        await dispatch(TodoAPI.create(values))
+      onSubmit={async (todoData, {setSubmitting}) => {
+        if (props.editMode) {
+          // Ask: how can I submit until request is done?
+          // Ask: Is it a correct way to handle both create and update actions?
+          await dispatch(TodoAPI.updateById({todoId, todoData}))
+        } else {
+          await dispatch(TodoAPI.create(todoData))
+        }
         setSubmitting(false)
       }}
     >
-      {({ submitForm }) => (
+      {({submitForm}) => (
         <Form>
           <Field
             component={TextField} name="name" label="Name" variant="outlined" size="small" margin="dense"/>
--- src/features/todos/TodoUpdatePage.js
+++ src/features/todos/TodoUpdatePage.js
@@ -1,14 +1,22 @@
-import {Button, TextField, Typography} from "@mui/material";
+import {Button, Typography} from "@mui/material";
 import * as React from "react";
-import {useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import {useDispatch, useSelector} from "react-redux";
 import {selectTodo} from "./todosSlice";
 import {TodoAPI} from "./todoAPI";
+import {TodoForm} from "./TodoCreatePage";
 
 const TodoUpdatePage = () => {
   const navigate = useNavigate();
+
+  const {todoId} = useParams();
+  const dispatch = useDispatch()
+  React.useEffect(() => {
+    dispatch(TodoAPI.fetchById(todoId))
+  }, []);
+  const todo = useSelector(selectTodo)
+
   return (
     <>
       <Button variant="contained" size="small" sx={{marginBottom: 3}} onClick={() => navigate(-1)}>
@@ -16,52 +24,9 @@ const TodoUpdatePage = () => {
       </Button>
       <Typography variant="h4" gutterBottom>Update Todo</Typography>
       <Paper elevation={3} sx={{maxWidth: 230, padding: 3}}>
-        <TodoUpdateForm/>
+        <TodoForm initial={todo} editMode={true}/>
       </Paper>
     </>
-  )
-}
-
-export const TodoUpdateForm = () => {
-  const dispatch = useDispatch()
-  const {todoId} = useParams();
-  React.useEffect(() => {
-    dispatch(TodoAPI.fetchById(todoId))
-  }, []);
-
-  const todo = useSelector(selectTodo)
-  const [name, setName] = useState(todo.name)
-  const [description, setDescription] = useState(todo.description)
-
-  const onNameChanged = e => setName(e.target.value)
-  const onDescriptionChanged = e => setDescription(e.target.value)
-
-  const handleSubmit = () => {
-    dispatch(TodoAPI.updateById({
-      todoId: todoId,
-      todoData: {name, description}
-    }))
-  }
-
-  // Ask: What is a proper way to do a validation?
-  const canSave = name.length >= 3 && description.length >= 3
-
-  // Ask: how to deduplicate with todoCreateForm
-  return (
-    <section>
-      {/*Ask: Should I bind handleSubmit to form itself or to a sumit button*/}
-      <form>
-        <TextField id="todoName" label="Name" variant="outlined" size="small" margin="dense"
-                   value={name} onChange={onNameChanged}/>
-        <br/>
-        <TextField id="todoDescription" label="Description" variant="outlined" size="small" margin="dense"
-                   value={description} onChange={onDescriptionChanged}/>
-        <br/>
-        <Button variant="contained" size="small" sx={{marginTop: 1}} onClick={handleSubmit} disabled={!canSave}>
-          Save
-        </Button>
-      </form>
-    </section>
   )
 }
 
